6a5d82c6cae1ffeff658142d63088cdf3de5b128
YanyaKhaki <yanina.khaki@gmail.com>
2014-07-22 15:48:04 +0300

    Заработало! о_о (функция openingRemainingData отвечает за показ сначала всех, а потом пропущенных вопросов)

--- scripts/script.js
+++ scripts/script.js
@@ -20,9 +20,7 @@
         //название теста
         app.addInfo('text', elem.question);
         //номер текущего вопроса
-        if ( numb > currentTest.length ){
-            numb = indexOfCurrQuest;
-        }
+        numb = indexOfCurrQuest;
         numb++;
         app.addInfo('currentNumb',numb);
         //картинка
@@ -71,141 +69,70 @@
 
 var indexOfCurrQuest = 0;
 var currentTest = null;
-var mySuperMas = [];
 var alreadyAnswered = 0;
 
-function turningOverMySuperMas() {
-    for (var newIter = 1; newIter < mySuperMas.length+1; newIter++) {
-        indexOfCurrQuest = mySuperMas[mySuperMas.length - newIter];
-        console.log(indexOfCurrQuest);
-        console.log(currentTest[indexOfCurrQuest]);
-        code.deleteQuestList();
-        code.openNextQuest(currentTest[indexOfCurrQuest])
-    }
-    mySuperMas = [];
-    console.log(mySuperMas);
-    //furtherActions();
-}
-
-function furtherActions(){
-    if(document.getElementsByClassName('numb')[0].innerHTML == indexOfCurrQuest) { //|| mySuperMas.length == 0
-        if (alreadyAnswered === currentTest.length) {
-            if (cor == currentTest.length || cor == currentTest.length - 1) {
-                alert('Йо-хо-хо, вы успешно прошли тест!\n' + cor + ' из ' + currentTest.length + '.\nИдем на главную.');
-                document.location.href = "index.html"
-            } else {
-                alert('Вы не прошли ._.\nРезультат: ' + cor + ' из ' + currentTest.length + '.\nИдем на главную.');
-                document.location.href = "index.html"
+function openingRemainingData() {
+    console.log(alreadyAnswered, currentTest.length, 'текущий в массиве:', indexOfCurrQuest);
+    var flag = false;
+    if (alreadyAnswered == currentTest.length) {
+        showTheResults()
+    } else {
+        if (currentTest.length == indexOfCurrQuest) {
+            for (var newSuperIndex = 0; newSuperIndex < currentTest.length; newSuperIndex++) {
+                if (!currentTest[newSuperIndex].answered) {
+                    code.deleteQuestList();
+                    indexOfCurrQuest = newSuperIndex;
+                    code.openNextQuest(currentTest[indexOfCurrQuest]);
+                    flag = true;
+                    break
+                }
             }
         } else {
-            if (confirm('Вы пропустили ' + mySuperMas.length + ' вопрос/a/ов. Продолжить выполнение теста?')) {
-                debugger;
-                for ( var superIndex = 0; superIndex < currentTest.length; superIndex++ ) {
-                    if (!currentTest[superIndex].answered) {
-                        code.deleteQuestList();
-                        code.openNextQuest(currentTest[superIndex]);
-                        indexOfCurrQuest = superIndex
-                    }
+            indexOfCurrQuest++;
+            for (var superIndex = indexOfCurrQuest; superIndex < currentTest.length; superIndex++) {
+                if (!currentTest[superIndex].answered) {
+                    flag = true;
+                    indexOfCurrQuest = superIndex;
+                    break
                 }
-                //turningOverMySuperMas()
-
-            } else {
-                document.location.href = "index.html"
             }
         }
-        return true;
+        if (flag == false) {
+            openingRemainingData();
+        } else {
+            code.deleteQuestList();
+            code.openNextQuest(currentTest[indexOfCurrQuest])
+        }
+    }
+}
+
+function showTheResults() {
+    var cor = document.getElementsByClassName('correctAn')[0].innerHTML;
+    if (cor == currentTest.length || cor == currentTest.length - 1) {
+        alert('Йо-хо-хо, вы успешно прошли тест!\n' + cor + ' из ' + currentTest.length + '.\nИдем на главную.');
+        document.location.href = "index.html"
     } else {
-        return false;
+        alert('Вы не прошли ._.\nРезультат: ' + cor + ' из ' + currentTest.length + '.\nИдем на главную.');
+        document.location.href = "index.html"
     }
 }
 
 var tests = document.getElementsByClassName('tests');
 for (var a = 0; a < tests.length; a++) {
     tests[a].addEventListener( "click", code.openTest, false )
-};
+}
 
 var nextQuest = document.getElementById('button');
 nextQuest.addEventListener( "click", function(){
-    indexOfCurrQuest++;
-    mySuperMas.push(indexOfCurrQuest);
-    console.log(mySuperMas);
-    if (!furtherActions()) {
-        code.deleteQuestList();
-        code.openNextQuest(currentTest[indexOfCurrQuest])
-    } else {
-        furtherActions()
-    }
+    openingRemainingData();
 }, false );
 
 var someAnswers = document.getElementById('questList');
 someAnswers.addEventListener( "click", function(evt){
     if (evt.target.className == 'answers') {
-        if (currentTest[indexOfCurrQuest].answered == true) {
-            alreadyAnswered++
-        }
         code.check(evt);
-        indexOfCurrQuest++;
-        if (!furtherActions()) {
-            currentTest[indexOfCurrQuest].answered = true;
-            console.log(alreadyAnswered);
-            code.deleteQuestList();
-            code.openNextQuest(currentTest[indexOfCurrQuest])
-        } else {
-            furtherActions()
-        }
+        currentTest[indexOfCurrQuest].answered = true;
+        alreadyAnswered++;
+        openingRemainingData();
     }
 }, false );
-
-/*delete indexOfCurrQuest;
-delete currentTest;
-delete mySuperMas*/
-
-// constants to define the title of the alert and button text.
-/*var ALERT_TITLE = "Oops!";
-var ALERT_BUTTON_TEXT = "Ok";
-
-// over-ride the alert method only if this a newer browser.
-// Older browser will see standard alerts
-if(document.getElementById) {
-    window.alert = function(txt) {
-        createCustomAlert(txt);
-    }
-}
-
-function createCustomAlert(txt) {
-    // shortcut reference to the document object
-    d = document;
-
-    // if the modalContainer object already exists in the DOM, bail out.
-    if(d.getElementById("modalContainer")) return;
-
-    // create the modalContainer div as a child of the BODY element
-    mObj = d.getElementsByTagName("body")[0].appendChild(d.createElement("div"));
-    mObj.id = "modalContainer";
-    // make sure its as tall as it needs to be to overlay all the content on the page
-    mObj.style.height = document.documentElement.scrollHeight + "px";
-
-    // create the DIV that will be the alert
-    alertObj = mObj.appendChild(d.createElement("div"));
-    alertObj.id = "alertBox";
-    // MSIE doesnt treat position:fixed correctly, so this compensates for positioning the alert
-    if(d.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
-    // center the alert box
-    alertObj.style.left = (d.documentElement.scrollWidth - alertObj.offsetWidth)/2 + "px";
-
-    // create an H1 element as the title bar
-    h1 = alertObj.appendChild(d.createElement("h1"));
-    h1.appendChild(d.createTextNode(ALERT_TITLE));
-
-    // create a paragraph element to contain the txt argument
-    msg = alertObj.appendChild(d.createElement("p"));
-    msg.innerHTML = txt;
-
-    // create an anchor element to use as the confirmation button.
-    btn = alertObj.appendChild(d.createElement("a"));
-    btn.id = "closeBtn";
-    btn.appendChild(d.createTextNode(ALERT_BUTTON_TEXT));
-    btn.href = "#";
-    // set up the onclick event to remove the alert when the anchor is clicked
-    btn.onclick = function() { document.getElementsByTagName("body")[0].removeChild(document.getElementById("modalContainer"));return false; }
-}*/

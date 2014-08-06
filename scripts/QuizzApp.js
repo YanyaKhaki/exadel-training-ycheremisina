d0704e446a8d67aa01b8603c69a992e67cfa925e
YanyaKhaki <yanina.khaki@gmail.com>
2014-08-06 14:58:40 +0300

    Изменена функция, работающая с LocalStorage

--- scripts/QuizzApp.js
+++ scripts/QuizzApp.js
@@ -4,33 +4,25 @@
         //this.smth = smth;
     }
 
-    QuizzApp.prototype.getToQuizzApp = function(currentTestIndex, currentQuestIndex, alreadyAnswered, correct, incorrect, alreadyDone) {
-        this.currentTestIndex = currentTestIndex;
-        this.currentQuestIndex = currentQuestIndex;
-        this.alreadyAnswered = alreadyAnswered;
-        this.correct = correct;
-        this.incorrect = incorrect;
-        this.alreadyDone = alreadyDone
-    };
-
-    QuizzApp.prototype.init = function(){
-
+    QuizzApp.prototype.init = function() {
         this.questModule = new QuizzModule();
         this.questModule.createQuest();
         this.questModule.answerQuest();
         this.questModule.nextQuest();
         this.questModule.mainPage();
-        this.questModule.Persist.getFromLocalStorage();
+        this.persist();
+        this.questModule.Persist.getFromLocalStorage()
+    };
 
-        //this.myRouter = new Router(this);
-
+    QuizzApp.prototype.persist = function(){
         if ( this.questModule.Persist.currTestIndex != -1 ) {
-            this.getToQuizzApp(this.questModule.Persist.currTestIndex, this.questModule.Persist.currQuestIndex, this.questModule.Persist.alrAnswered, this.questModule.Persist.corr, this.questModule.Persist.incorr, this.questModule.Persist.alrDone);
+            //debugger;
+            this.questModule.getToQuizzApp(this.questModule.Persist.currTestIndex, this.questModule.Persist.currQuestIndex, this.questModule.Persist.alrAnswered, this.questModule.Persist.corr, this.questModule.Persist.incorr, this.questModule.Persist.alrDone);
+            console.log(this.questModule.currentTestIndex);
             this.questModule.currentTest = quizData[this.questModule.currentTestIndex].questions;
             this.questModule.hideElements('listQ', 'question');
             this.questModule.openNextQuest(this.questModule.currentTest[this.questModule.currentQuestIndex]);
         }
-
     };
 
     root.QuizzApp = QuizzApp;

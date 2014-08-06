30226615bfe37b335190432baa0f04bc4317dadc
YanyaKhaki <yanina.khaki@gmail.com>
2014-08-06 14:58:01 +0300

    Написана функция, зануляющая значения статистики, подключен роутинг

--- scripts/QuizzModule.js
+++ scripts/QuizzModule.js
@@ -15,45 +15,45 @@
         this.$goBack = $('.back');
         this.statistics = new Stats(this);
         this.Persist = new PersistanceModule();
+        this.myRouter = new Router(this);
+        //this.myRouter.onURLChange();
     }
 
     QuizzModule.prototype.createQuest = function () {
+        this.Persist.cleanStatistics();
         this.$testList.on("click", { param: this }, function (event) {
             event.data.param.currentTestIndex = parseInt(event.target.getAttribute("data-test-id"), 10);
-            event.data.param.openTest(event);
-            //self.myRouter.getStats();
-            //self.myRouter.createURL()
+            event.data.param.myRouter.getStats(event.data.param.currentTestIndex, event.data.param.currentQuestIndex);
+            event.data.param.myRouter.createURL();
+            event.data.param.openTest(event)
         })
     };
 
     QuizzModule.prototype.mainPage = function () {
-        this.$goBack.on("click", { param: this }, function (evt) {
-            event.data.param.Persist.cleanStatistics();
-            event.data.param.hideElements('question','listQ');
-            event.data.param.currentQuestIndex = 0;
-            event.data.param.currentTestIndex = -1;
-            event.data.param.currentTest = 0;
-            event.data.param.alreadyAnswered = 0;
-            event.data.param.correct = 0;
-            event.data.param.incorrect = 0;
+        this.$goBack.on("click", { param: this }, function (event) {
+            var self = event.data.param;
+            self.Persist.cleanStatistics();
+            self.hideElements('question','listQ');
+            self.bringToNought();
             self.Persist.getStatistics(self.questModule.currentTestIndex, self.questModule.currentQuestIndex, self.questModule.alreadyAnswered, self.questModule.correct, self.questModule.incorrect, self.questModule.alreadyDone);
-            event.data.param.getToQuizzApp(event.data.param.Persist.currTestIndex, event.data.param.Persist.currQuestIndex, event.data.param.Persist.alrAnswered, event.data.param.Persist.corr, event.data.param.Persist.incorr, event.data.param.Persist.alrDone);
-            event.data.param.statistics.addInfoToStats(event.data.param.currentQuestIndex, '0');
-            //self.myRouter.getStats(self.questModule.currentTestIndex, self.questModule.currentQuestIndex);
-            //self.myRouter.createURL()
+            self.getToQuizzApp(self.Persist.currTestIndex, self.Persist.currQuestIndex, self.Persist.alrAnswered, self.Persist.corr, self.Persist.incorr, self.Persist.alrDone);
+            self.statistics.addInfoToStats(self.currentQuestIndex, '0');
+            self.myRouter.getStats(self.currentTestIndex, self.currentQuestIndex);
+            //self.myRouter.createURL();
             window.location.hash = '';
-            evt.preventDefault();
+            event.preventDefault()
         })
     };
 
     QuizzModule.prototype.answerQuest = function () {
         this.$someAnswers.on("click", { param: this }, function (evt) {
-            evt.data.param.check(evt);
-            evt.data.param.currentTest[evt.data.param.currentQuestIndex].answered = true;
-            evt.data.param.alreadyAnswered++;
-            evt.data.param.openRemainingData();
-            //self.myRouter.getStats(self.questModule.currentTestIndex, self.questModule.currentQuestIndex);
-            //self.myRouter.createURL()
+            var self = evt.data.param;
+            self.check(evt);
+            self.currentTest[self.currentQuestIndex].answered = true;
+            self.alreadyAnswered++;
+            self.openRemainingData();
+            self.myRouter.getStats(self.currentTestIndex, self.currentQuestIndex);
+            self.myRouter.createURL()
         })
     };
 
@@ -64,10 +64,9 @@
     };
 
     QuizzModule.prototype.openTest = function (evt) {
-        console.log(this.$tests);
-        //this.myRouter.cleanURL();
-        //this.myRouter.getStats();
-        //this.myRouter.createURL();
+        this.myRouter.cleanURL();
+        this.myRouter.getStats(this.currentTestIndex, this.currentQuestIndex);
+        this.myRouter.createURL();
         if (this.alreadyDone.length === quizData.length) {
             alert('Вы прошли все тесты')
         }
@@ -101,13 +100,14 @@
             node.setAttribute('class', 'answers');
             node.setAttribute('check-answer-id', i + 1);
             node.appendChild(textnode);
-            document.getElementById('questList').appendChild(node)
+            document.getElementById('questList').appendChild(node);
+            //node.style.background = '#CAFF70';
         }
         this.addInfo('testName', quizData[this.currentTestIndex].title);
         this.statistics.addInfoToStats(this.currentQuestIndex + 1, this.currentTest.length);
-        //this.myRouter.getStats(this.currentTestIndex, this.currentQuestIndex);
-        //this.myRouter.createURL();
-        this.Persist.getStatistics(this.currentTestIndex, this.currentQuestIndex, this.alreadyAnswered, this.correct, this.incorrect, this.alreadyDone);
+        this.myRouter.getStats(this.currentTestIndex, this.currentQuestIndex);
+        this.myRouter.createURL();
+        this.Persist.getStatistics(this.currentTestIndex, this.currentQuestIndex, this.alreadyAnswered, this.correct, this.incorrect, this.alreadyDone)
     };
 
     QuizzModule.prototype.check = function (myAnswer) {
@@ -116,35 +116,26 @@
             this.correct++;
             this.addInfo('correctAn', this.correct);
             myAnswer.target.style.backgroundColor = 'green';
+            /*$.fn.jAlert({
+                'title': 'Верно!:)',
+                'message': "Ура-ура!",
+                'theme': 'success',
+                'size': 'medium'
+            });*/
             alert('Верно!:)')
         }
         else {
             this.incorrect++;
             this.addInfo('incorrectAn', this.incorrect);
             myAnswer.target.style.backgroundColor = 'red';
+            /*$.fn.jAlert({
+                'title': 'Неверно',
+                'message': "Правильный ответ: '" + this.currentTest[this.currentQuestIndex].answers[currentCorrectAnsw - 1] + "'",
+                'theme': 'error',
+                'size': 'medium'
+            });*/
             alert("Неверно.\nПравильный ответ: '" + this.currentTest[this.currentQuestIndex].answers[currentCorrectAnsw - 1] + "'")
         }
-    };
-
-    QuizzModule.prototype.deleteQuestList = function () {
-        var element = document.getElementById("questList");
-        while (element.firstChild) {
-            element.removeChild(element.firstChild)
-        }
-    };
-
-    QuizzModule.prototype.addInfo = function (className, info) {
-        //$('."' + className + '"')[0].innerHTML = info
-       document.getElementsByClassName(className)[0].innerHTML = info
-    };
-
-    QuizzModule.prototype.hideElements = function (class1, class2) {
-        //var elem1 = $('."' + class1 + '"')[0];
-        //var elem2 = $('."' + class2 + '"')[0];
-        var elem1 = document.getElementsByClassName(class1)[0];
-        var elem2 = document.getElementsByClassName(class2)[0];
-        elem1.style.display = 'none';
-        elem2.style.display = 'block'
     };
 
     QuizzModule.prototype.openRemainingData = function () {
@@ -189,23 +180,58 @@
         this.alreadyDone.push(this.currentTestIndex + 1);
         var cor = document.getElementsByClassName('correctAn')[0].innerHTML;
         if (cor == this.currentTest.length || cor == this.currentTest.length - 1) {
-            alert('Йо-хо-хо, вы успешно прошли тест!\n' + cor + ' из ' + this.currentTest.length + '.\nИдем на главную.')
+            alert('Йо-хо-хо, вы успешно прошли тест!\nПравильных: ' + cor + ' из ' + this.currentTest.length + '.\nИдем на главную.')
         } else {
             alert('Вы не прошли ._.\nРезультат: ' + cor + ' из ' + this.currentTest.length + '.\nИдем на главную.')
         }
         this.hideElements('question', 'listQ');
         this.$tests[this.alreadyDone[this.alreadyDone.length - 1] - 1].style.listStyleImage = 'url(pics/tick.png)';
         this.$tests[this.alreadyDone[this.alreadyDone.length - 1] - 1].setAttribute('data-test-id', '-1');
+        this.bringToNought();
+        this.Persist.getStatistics(this.currentTestIndex, this.currentQuestIndex, this.alreadyAnswered, this.correct, this.incorrect, this.alreadyDone);
+        this.getToQuizzApp(this.Persist.currTestIndex, this.Persist.currQuestIndex, this.Persist.alrAnswered, this.Persist.corr, this.Persist.incorr, this.Persist.alrDone);
+        this.statistics.addInfoToStats(this.currentQuestIndex, '0');
+        this.myRouter.getStats(this.currentTestIndex, this.currentQuestIndex);
+        this.myRouter.cleanURL();
+    };
+
+    QuizzModule.prototype.getToQuizzApp = function(currentTestIndex, currentQuestIndex, alreadyAnswered, correct, incorrect, alreadyDone) {
+        this.currentTestIndex = currentTestIndex;
+        this.currentQuestIndex = currentQuestIndex;
+        this.alreadyAnswered = alreadyAnswered;
+        this.correct = correct;
+        this.incorrect = incorrect;
+        this.alreadyDone = alreadyDone
+    };
+
+    QuizzModule.prototype.deleteQuestList = function () {
+        var element = document.getElementById("questList");
+        while (element.firstChild) {
+            element.removeChild(element.firstChild)
+        }
+    };
+
+    QuizzModule.prototype.addInfo = function (className, info) {
+        //$('."' + className + '"')[0].innerHTML = info
+       document.getElementsByClassName(className)[0].innerHTML = info
+    };
+
+    QuizzModule.prototype.hideElements = function (class1, class2) {
+        //var elem1 = $('."' + class1 + '"')[0];
+        //var elem2 = $('."' + class2 + '"')[0];
+        var elem1 = document.getElementsByClassName(class1)[0];
+        var elem2 = document.getElementsByClassName(class2)[0];
+        elem1.style.display = 'none';
+        elem2.style.display = 'block'
+    };
+
+    QuizzModule.prototype.bringToNought = function(){
         this.currentQuestIndex = 0;
         this.currentTestIndex = -1;
         this.currentTest = 0;
         this.alreadyAnswered = 0;
         this.correct = 0;
         this.incorrect = 0;
-        this.Persist.getStatistics(this.currentTestIndex, this.currentQuestIndex, this.alreadyAnswered, this.correct, this.incorrect, this.alreadyDone);
-        this.getToQuizzApp(this.Persist.currTestIndex, this.Persist.currQuestIndex, this.Persist.alrAnswered, this.Persist.corr, this.Persist.incorr, this.Persist.alrDone);
-        this.statistics.addInfoToStats(this.currentQuestIndex, '0');
-        window.location.hash = '';
     };
 
     root.QuizzModule = QuizzModule;

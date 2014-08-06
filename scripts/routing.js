0bfaf9e567ccf1bfbcfe2ff068d3893e7f6fb7a7
YanyaKhaki <yanina.khaki@gmail.com>
2014-08-06 14:59:24 +0300

    Написана функция, строящая вопрос по заданному хэшу

--- scripts/routing.js
+++ scripts/routing.js
@@ -8,58 +8,44 @@
 
     Router.prototype.getStats = function(currTest, currQuest){
         this.currTest = currTest + 1;
-        this.currQuest = currQuest + 1;
+        this.currQuest = currQuest + 1
     };
 
     Router.prototype.createURL = function() {
-        window.location.hash = '#test/'+ this.currTest + '/' + this.currQuest + '/';
+        window.location.hash = '#test/'+ this.currTest + '/' + this.currQuest;
+        //this.app.openRemainingData()
     };
 
     Router.prototype.cleanURL = function() {
-        window.location.hash = '';
+        window.location.hash = ''
+    };
+
+    Router.prototype.createQuestFromURL = function(myTestIndex, myQuestIndex) {
+        this.currTest = myTestIndex;
+        this.currQuest = myQuestIndex;
+        this.createURL();
+        this.app.currentTestIndex = this.currTest;
+        this.app.currentQuestIndex = this.currQuest;
+        this.currentTest = quizData[myTestIndex].questions;
+        this.app.openRemainingData()
     };
 
     Router.prototype.onURLChange = function() {
         //debugger;
-        var myTestIndex = window.location.hash.charAt(6);
-        var myQuestIndex = window.location.hash.charAt(8);
+        var router = window.location.hash.split('/');
+        var myTestIndex = router[1];
+        var myQuestIndex = router[2];
         console.log(myTestIndex,myQuestIndex);
         if ( 0 < myTestIndex && myTestIndex < quizData.length + 1 ) {
             if ( 0 < myQuestIndex && myQuestIndex < this.app.currentTest.length ) {
-                this.currTest = myTestIndex;
-                this.currQuest = myQuestIndex;
-                this.createURL();
-                this.app.currentTestIndex = this.currTest;
-                this.app.currentQuestIndex = this.currQuest;
-                this.currentTest = quizData[myTestIndex].questions;
-                this.app.openRemainingData();
+                this.createQuestFromURL(myTestIndex, myQuestIndex);
             } else {
-                this.currTest = myTestIndex;
-                this.currQuest = 1;
-                this.createURL();
-                this.app.currentTestIndex = this.currTest;
-                this.app.currentQuestIndex = this.currQuest;
-                this.currentTest = quizData[myTestIndex].questions;
-                //console.log(this.app.currentQuestIndex);
-                this.app.openRemainingData();
+                this.createQuestFromURL(myTestIndex, 1);
             }
         } else {
-            var elem1 = document.getElementsByClassName('question')[0];
-            var elem2 = document.getElementsByClassName('listQ')[0];
-            elem1.style.display = 'none';
-            elem2.style.display = 'block';
-            this.app.currentQuestIndex = 0;
-            this.app.currentTestIndex = 0;
-            this.app.currentTest = 0;
-            this.app.alreadyAnswered = 0;
-            this.app.correct = 0;
-            this.app.incorrect = 0;
-            //this.Persist.cleanStatistics();
-            this.app.addInfo('correctAn', this.app.correct);
-            this.app.addInfo('incorrectAn', this.app.incorrect);
-            this.app.addInfo('currentNumb', this.app.currentQuestIndex);
-            this.app.addInfo('numb','0');
-            //window.location.href = 'index.html'
+            this.app.hideElements('question', 'listQ');
+            this.app.bringToNought();
+            this.app.addInfoToStats(this.app.questModule.currentQuestIndex,'0')
         }
     };
 

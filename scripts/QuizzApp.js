21db69000f178e6f48dfa11baf64f8e8aef57cd6
YanyaKhaki <yanina.khaki@gmail.com>
2014-08-07 13:13:51 +0300

    Повешен обработчик (onhashchange)

--- scripts/QuizzApp.js
+++ scripts/QuizzApp.js
@@ -11,13 +11,16 @@
         this.questModule.nextQuest();
         this.questModule.mainPage();
         this.persist();
-        this.questModule.Persist.getFromLocalStorage()
+        this.questModule.myPersist.getFromLocalStorage();
+        $(window).on('hashchange', { param: this }, function(evt){
+            evt.data.param.questModule.myRouter.onURLChange();
+        });
     };
 
     QuizzApp.prototype.persist = function(){
-        if ( this.questModule.Persist.currTestIndex != -1 ) {
+        if ( this.questModule.myPersist.currTestIndex != -1 ) {
             //debugger;
-            this.questModule.getToQuizzApp(this.questModule.Persist.currTestIndex, this.questModule.Persist.currQuestIndex, this.questModule.Persist.alrAnswered, this.questModule.Persist.corr, this.questModule.Persist.incorr, this.questModule.Persist.alrDone);
+            this.questModule.getToQuizzApp(this.questModule.myPersist.currTestIndex, this.questModule.myPersist.currQuestIndex, this.questModule.myPersist.alrAnswered, this.questModule.myPersist.corr, this.questModule.myPersist.incorr, this.questModule.myPersist.alrDone);
             console.log(this.questModule.currentTestIndex);
             this.questModule.currentTest = quizData[this.questModule.currentTestIndex].questions;
             this.questModule.hideElements('listQ', 'question');
